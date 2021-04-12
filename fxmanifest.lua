98f8a31fea715401e9ed4217de8f269fea494393
ItsWrath <34563489+ItsWrath@users.noreply.github.com>
2021-04-12 13:38:09 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Badger Door Lock'
+
+version '1.4.0'
+
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
